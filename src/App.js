180027474382f0d1fd1f294965b7f885b22e3184
Akshay Vr <avr02339@gmail.com>
2023-03-03 12:48:05 +0530

    3rd video completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react"
+import Employee from "./Employee"
 function App() {
+  //Array destructuring
+  //Eg:const arr=[1,2,3,4]
+  //Eg:const [one,two,three,four]=arr
+  //now one=1 two=2 three=3...
+  //setCount is a function
+  //useState(0)=>assume that setting a variable and giving it a value 0
+  const [count,setCount]=useState(0)
+  
+  const addCount=()=>{
+    setCount(count+1)
+    console.log(count)
+  }
+ let emp=[{name:"Akshay" ,age:19},{name:"Anoop",age:21},{name:"musk",age:22}]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div >
+      <button onClick={addCount}>Add</button>
+      {
+        emp.map((obj,index)=>{
+          return(
+            <Employee key={index} {...obj}/>
+          )
+          //we can remove the { } in arrow function and what ever we write aafter that will be returned
+
+
+        })
+      }
+   </div>
   );
 }
 
