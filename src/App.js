f9472b97dd3869e1fb8c75c57ea893c30c1f5a39
Akshay Vr <avr02339@gmail.com>
2023-03-06 07:55:13 +0530

    updating & mounting/unmounting

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import { useState } from "react"
-import Employee from "./Employee"
+import Counter from "./Counter";
+import { useState } from "react";
 function App() {
-  //Array destructuring
-  //Eg:const arr=[1,2,3,4]
-  //Eg:const [one,two,three,four]=arr
-  //now one=1 two=2 three=3...
-  //setCount is a function
-  //useState(0)=>assume that setting a variable and giving it a value 0
-  const [count,setCount]=useState(0)
-  
-  const addCount=()=>{
-    setCount(count+1)
-    console.log(count)
-  }
- let emp=[{name:"Akshay" ,age:19},{name:"Anoop",age:21},{name:"musk",age:22}]
-  return (
+
+  const [state,setState]=useState(false)
+   return (
    <div >
-      <button onClick={addCount}>Add</button>
-      {
-        emp.map((obj,index)=>{
-          return(
-            <Employee key={index} {...obj}/>
-          )
-          //we can remove the { } in arrow function and what ever we write aafter that will be returned
+       <h1 onClick={()=>setState(!state)}>Hello World</h1>
+      { state ? <Counter/> : null}
 
-
-        })
-      }
    </div>
   );
 }
